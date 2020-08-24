f0d54e99055c3d1f8c98af452187716e8daf4477
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-24 20:22:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -102,8 +102,7 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      ORDR BY students.id
-      LIMIT 1
+      ORDR BY students.id LIMIT 1
     SQL
 
     DB[:conn].execute(sql).map do |row|
