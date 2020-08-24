0b102a923a46b0b787a968eb53312c052893acb9
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-24 19:58:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ end
 
 def self.all_students_in_grade_9
           sql = <<-SQL
-            SELECT COUNT(*) FROM songs WHERE grade = 9 
+            SELECT COUNT(*) FROM songs WHERE grade = 9; 
         SQL
 
         DB[:conn].execute(sql, name).map do |row|
