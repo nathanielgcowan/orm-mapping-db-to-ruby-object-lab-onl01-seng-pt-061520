ff74853d8a25bf447b4197852187b0a1692f7ab5
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-24 20:20:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -103,11 +103,12 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
+      LIMIT 1
     SQL
 
     DB[:conn].execute(sql, number).map do |row|
       self.new_from_db(row)
-    end
+    end.first
   end
   
   def self.all_students_in_grade_X(grade)
