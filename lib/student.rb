4b0976ed2349aee5514fa1ce0dcc1988ee404895
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-24 20:24:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,15 +98,15 @@ class Student
     end
   end
   
-  def self.first_student_in_grade_10(number)
+  def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade = 10
-      ORDR BY students.id LIMIT 1
+      ORDER BY students.id LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, number).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
   end
