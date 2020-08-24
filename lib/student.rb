f5e9d4d38b891899cfb8b4cbc52e6db66853fb3b
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-24 20:20:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,6 +98,18 @@ class Student
     end
   end
   
+    def self.first_student_in_grade_10(number)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+    SQL
+
+    DB[:conn].execute(sql, number).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
   def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT *
