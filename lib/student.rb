5c6632a1a387f04f8492a69064d282d688923876
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-24 20:06:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,6 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
-end
 
   def self.all_students_in_grade_9
     sql = <<-SQL
@@ -71,4 +70,4 @@ end
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
-  end
+end
