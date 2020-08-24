1098987883dfbe3ee7ed6de5fec269b9df78c8f4
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-24 20:05:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,14 +61,14 @@ class Student
   end
 end
 
-def self.all_students_in_grade_9
+  def self.all_students_in_grade_9
     sql = <<-SQL
       SELECT COUNT(*)
-      FROM students 
+      FROM students
       WHERE grade = 9;
     SQL
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
+    end
   end
-end
