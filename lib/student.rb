c471325c48f60d3fdcb15968c53b9def070edf71
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-24 20:14:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,7 +86,7 @@ class Student
   
   def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
-      SELECT COUNT(*)
+      SELECT *
       FROM students
       WHERE grade = 10;
       LIMIT ?
